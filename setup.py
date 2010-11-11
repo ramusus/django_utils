967ab4306bf88a208f7a135545fd88f17fadfdba
ram <ramusus@gmail.com>
2010-11-12 02:29:07 +0300

    added setup.py, README

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django_utils',
+    version=__import__('django_utils').__version__,
+    description='Set of tools and useful additions for Django Framework',
+    long_description=open('README').read(),
+    author='ramusus',
+    author_email='ramusus@gmail.com',
+    url='https://github.com/ramusus/django_utils',
+    download_url='https://github.com/ramusus/django_utils/downloads',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False, # because we're including media that Django needs
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
