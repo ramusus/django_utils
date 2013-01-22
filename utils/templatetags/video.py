8ba28efdbc99a7e8d28324990569e8cc079816fb
ram <ramusus@gmail.com>
2013-01-22 16:24:32 +0530

    added kinopoisk_tag to templatetags, improve other tags

--- utils/templatetags/video.py
+++ utils/templatetags/video.py
@@ -2,32 +2,52 @@ from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 from django import template
 import re
+import urllib2
 
 register = template.Library()
 
 youtube_url_pattern = r'^(?:http://)?(?:www\.)?(?:youtube\.com/(?:watch\?v=|v/))?(?P<id>[A-Za-z0-9\-=_]{11})'
 ivi_url_pattern = r'^(?:http://)?(?:www\.)?ivi\.ru/video/player/\?videoId=(?P<id>[0-9]+)'
+kinopoisk_url_pattern = r'^(?:http://)?(?:tr\.)?kinopoisk\.ru/(?P<id>.+)'
 
 def youtube_tag(video_id, width, height):
     return '''
-    <object width="%s" height="%s">
-        <param name="movie" value="http://www.youtube.com/v/%s?fs=1"></param>
+    <object width="%(width)s" height="%(height)s">
+        <param name="movie" value="http://www.youtube.com/v/%(video_id)s?fs=1"></param>
         <param name="allowFullScreen" value="true"></param>
-        <embed src="http://www.youtube.com/v/%s?fs=1" type="application/x-shockwave-flash" allowfullscreen="true" wmode="opaque" width="%s" height="%s"></embed>
+        <embed src="http://www.youtube.com/v/%(video_id)s?fs=1" type="application/x-shockwave-flash" allowfullscreen="true" wmode="opaque" width="%(width)s" height="%(height)s"></embed>
     </object>
-    ''' % (width, height, video_id, video_id, width, height)
+    ''' % locals()
 
 def ivi_tag(video_id, width, height):
     return '''
-    <object id="DigitalaccessVideoPlayer" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="%s" height="%s">
-        <param name="movie" value="http://www.ivi.ru/video/player/?videoId=%s&_isB2C=1" />
-        <param name="allowScriptAccess" value="sameDomain" />
-        <param name="allowFullScreen" value="true" />
-        <param name="bgcolor" value="#000000" />
-        <param name="wmode" value="opaque" />
-        <embed src="http://www.ivi.ru/video/player/?videoId=%s" quality="high" allowscriptaccess="sameDomain" allowfullscreen="true" wmode="opaque"  width="%s" height="%s" type="application/x-shockwave-flash"></embed>
+    <object id="DigitalaccessVideoPlayer" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="%(width)s" height="%(height)s">
+        <param name="movie" value="http://www.ivi.ru/video/player/?videoId=%video_ids&_isB2C=1"></param>
+        <param name="allowScriptAccess" value="sameDomain"></param>
+        <param name="allowFullScreen" value="true"></param>
+        <param name="bgcolor" value="#000000"></param>
+        <param name="wmode" value="opaque"></param>
+        <embed src="http://www.ivi.ru/video/player/?videoId=%video_ids" quality="high" allowscriptaccess="sameDomain" allowfullscreen="true" wmode="opaque"  width="%(width)s" height="%(height)s" type="application/x-shockwave-flash"></embed>
     </object>
-    ''' % (width, height, video_id, video_id, width, height)
+    ''' % locals()
+
+def kinopoisk_tag(video_file, preview_file, video_url, width, height):
+
+    preview_id = re.findall(r'_(\d+).jpg$', preview_file)[0]
+    preview_file = 'http://tr.kinopoisk.ru/' + preview_file
+    video_id = re.findall(r'^(\d+)', video_file)[0]
+    video_file = re.findall(r'/(.+)', video_file)[0]
+    video_file = 'http://www.kinopoisk.ru/gettrailer.php' + urllib2.quote('?trid=%s&film=%s&tid=%s' % (preview_id, video_id, video_file))
+
+    return '''
+    <object width="%(width)s" height="%(height)s">
+        <param name="movie" value="http://tr.kinopoisk.ru/js/jw/player-licensed.swf"></param>
+        <param name="wmode" value="transparent"></param>
+        <param name="allowFullScreen" value="true"></param>
+        <param name="flashVars" value="image=%(preview_file)s&%(video_url)s&file=%(video_file)s&skin=http://tr.kinopoisk.ru/js/jw/overlay.swf&controlbar=over&frontcolor=ffffff&lightcolor=ff6600&stretching=unified"></param>
+        <embed src="http://tr.kinopoisk.ru/js/jw/player-licensed.swf" type="application/x-shockwave-flash" wmode="transparent" width="%(width)s" height="%(height)s" allowFullScreen="true" flashVars="image=%(preview_file)s&%(video_url)s&file=%(video_file)s&skin=http://tr.kinopoisk.ru/js/jw/overlay.swf&controlbar=over&frontcolor=ffffff&lightcolor=ff6600&stretching=unified"></embed>
+    </object>
+    ''' % locals()
 
 @register.filter
 @stringfilter
@@ -56,7 +76,6 @@ def ivi(url, sizes='465x278'):
     video_tag = ivi_tag(video_id, width, height)
     return mark_safe(video_tag)
 ivi.is_safe = True # Don't escape HTML
-
 
 # from here http://www.djangosnippets.org/snippets/212/
 
