c84f7cf130927f05ed076c0d33931c5e47f45686
ramusus <ramusus@gmail.com>
2013-04-24 00:05:56 +0400

    added GenericFieldsQuerySet

--- utils/models.py
+++ utils/models.py
@@ -1,35 +1,71 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.db.models.query import QuerySet
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
-class GenericFieldsManager(models.Manager):
+class ConvertGenericMixin():
+    def convert_generic(self, args, kwargs):
+        for field in self.model._meta.virtual_fields:
+            if isinstance(field, generic.GenericForeignKey):
+                if field.name in kwargs:
+                    content_object = kwargs.pop(field.name)
+                    kwargs[field.ct_field] = ContentType.objects.get_for_model(content_object)
+                    kwargs[field.fk_field] = content_object.pk
+                if field.name in args:
+                    args = list(args)
+                    args.remove(field.name)
+                    args += [field.ct_field, field.fk_field]
+                    args = tuple(args)
+        return args, kwargs
+
+class GenericFieldsManager(models.Manager, ConvertGenericMixin):
     '''
     Subclass for filter by GenericForeignKey field in filter, exclude requests
     '''
-    def convert_generic(self, kwargs):
-        for field in self.model._meta.virtual_fields:
-            if isinstance(field, generic.GenericForeignKey) and field.name in kwargs:
-                content_object = kwargs.pop(field.name)
-                kwargs[field.ct_field] = ContentType.objects.get_for_model(content_object)
-                kwargs[field.fk_field] = content_object.pk
-        return kwargs
-
     def filter(self, *args, **kwargs):
-        kwargs = self.convert_generic(kwargs)
+        args, kwargs = self.convert_generic(args, kwargs)
         return super(GenericFieldsManager, self).filter(*args, **kwargs)
 
     def exclude(self, *args, **kwargs):
-        kwargs = self.convert_generic(kwargs)
+        args, kwargs = self.convert_generic(args, kwargs)
         return super(GenericFieldsManager, self).exclude(*args, **kwargs)
 
     def get_or_create(self, *args, **kwargs):
-        kwargs = self.convert_generic(kwargs)
+        args, kwargs = self.convert_generic(args, kwargs)
         return super(GenericFieldsManager, self).get_or_create(*args, **kwargs)
 
     def get(self, *args, **kwargs):
-        kwargs = self.convert_generic(kwargs)
+        args, kwargs = self.convert_generic(args, kwargs)
         return super(GenericFieldsManager, self).get(*args, **kwargs)
+
+    def distinct(self, *args, **kwargs):
+        args, kwargs = self.convert_generic(args, kwargs)
+        return super(GenericFieldsManager, self).distinct(*args, **kwargs)
+
+class GenericFieldsQuerySet(QuerySet, ConvertGenericMixin):
+    '''
+    Subclass for filter by GenericForeignKey field in filter, exclude requests
+    '''
+    def filter(self, *args, **kwargs):
+        args, kwargs = self.convert_generic(args, kwargs)
+        return super(GenericFieldsQuerySet, self).filter(*args, **kwargs)
+
+    def exclude(self, *args, **kwargs):
+        args, kwargs = self.convert_generic(args, kwargs)
+        return super(GenericFieldsQuerySet, self).exclude(*args, **kwargs)
+
+    def get_or_create(self, *args, **kwargs):
+        args, kwargs = self.convert_generic(args, kwargs)
+        return super(GenericFieldsQuerySet, self).get_or_create(*args, **kwargs)
+
+    def get(self, *args, **kwargs):
+        args, kwargs = self.convert_generic(args, kwargs)
+        return super(GenericFieldsQuerySet, self).get(*args, **kwargs)
+
+    def distinct(self, *args, **kwargs):
+        args, kwargs = self.convert_generic(args, kwargs)
+        return super(GenericFieldsQuerySet, self).distinct(*args, **kwargs)
 
 def ModelQuerySetManager(ManagerBase=models.Manager):
     '''
@@ -46,8 +82,9 @@ def ModelQuerySetManager(ManagerBase=models.Manager):
         def get_query_set(self):
             return self.model.QuerySet(self.model)
 
-        def __getattr__(self, name):
-            return getattr(self.get_query_set(), name)
+        # this method cause memory overfull in admin
+#        def __getattr__(self, name):
+#            return getattr(self.get_query_set(), name)
 
     return Manager()
 
