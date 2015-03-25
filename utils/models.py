de70ee86d08808ed4b3927c13bcd0a0e3ef25e02
ramusus <ramusus@gmail.com>
2015-03-26 00:48:01 +0700

    fixed empty queryset case

--- utils/models.py
+++ utils/models.py
@@ -216,8 +216,9 @@ class GFKQuerySet(QuerySet):
 # from here https://djangosnippets.org/snippets/1949/
 import gc
 
+
 def queryset_iterator(queryset, chunksize=1000):
-    '''''
+    """
     Iterate over a Django Queryset ordered by the primary key
 
     This method loads a maximum of chunksize (default: 1000) rows in it's
@@ -226,9 +227,12 @@ def queryset_iterator(queryset, chunksize=1000):
     classes.
 
     Note that the implementation of the iterator does not support ordered query sets.
-    '''
+    """
     pk = 0
-    last_pk = queryset.order_by('-pk')[0].pk
+    try:
+        last_pk = queryset.order_by('-pk')[0].pk
+    except IndexError:
+        return
     queryset = queryset.order_by('pk')
     while pk < last_pk:
         for row in queryset.filter(pk__gt=pk)[:chunksize]:
